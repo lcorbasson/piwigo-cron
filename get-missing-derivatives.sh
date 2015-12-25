7d28d299bfe2389a859faf24c03d75ad93ed8acb
Loic Corbasson <loic.corbasson@gmail.com>
2015-12-25 05:44:50 +0100

    Further small changes for better code legibility.

--- get-missing-derivatives.sh
+++ get-missing-derivatives.sh
@@ -13,6 +13,8 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 DURATION=3600 # in seconds
 URLS_PER_LOOP=50
 
+PIWIGO_URL="${PIWIGO_URL%/}"	# remove trailing slash to avoid unnecessary HTTP 301's
+
 get_urls () {
     cookie_file="$(mktemp)"
 
@@ -24,8 +26,8 @@ get_urls () {
       "${PIWIGO_URL}/ws.php?format=json"
 
     # Get a json of the urls to visit with the "pwg.getMissingDerivatives" method.
-    json=$(curl --silent --cookie "$cookie_file" \
-      "${PIWIGO_URL}/ws.php?format=json&method=pwg.getMissingDerivatives&max_urls=${URLS_PER_LOOP}")
+    json="$(curl --silent --cookie "$cookie_file" \
+      "${PIWIGO_URL}/ws.php?format=json&method=pwg.getMissingDerivatives&max_urls=${URLS_PER_LOOP}")"
 
     rm -f "$cookie_file"
 
@@ -38,8 +40,7 @@ for url in j['result']['urls']:
 "
 
     # Extracting urls with Python and "json" module.
-    if echo "$python_script" | python
-    then
+    if echo "$python_script" | python; then
         return 0
     else
         return 1
@@ -51,16 +52,13 @@ for url in j['result']['urls']:
 begin=$(date "+%s")
 loop_number=1
 
-while true
-do
+while true; do
     echo ">>> Loop number $loop_number"
     loop_number=$((loop_number + 1))
 
     # curl on each urls to generate pictures.
-    if urls=$(get_urls)
-    then
-        for url in $urls
-        do
+    if urls="$(get_urls)"; then
+        for url in $urls; do
             echo "$url"
             curl --silent --output /dev/null "$url"
         done
@@ -71,8 +69,7 @@ do
     fi
 
     # If there is no url, it's finished.
-    if [ -z "$urls" ]
-    then
+    if [ -z "$urls" ]; then
         echo "There is no url, end of the script."
         exit 0
     fi
@@ -81,9 +78,9 @@ do
     elapsed_time=$((now - begin))
 
     # If the timeout is exceeded, it's finished.
-    if [ $elapsed_time -ge $DURATION ]
-    then
+    if [ $elapsed_time -ge $DURATION ]; then
         echo "Timeout, end of the script."
         exit 0
     fi
 done
+
